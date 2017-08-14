d575c1bdfdacc778acba85177b888bdb92e68bab
Jim Ramsay <i.am@jimramsay.com>
2017-08-14 11:57:48 -0400

    Reorganize and recategorize preferences
    Signed-off-by: Jim Ramsay <i.am@jimramsay.com>

--- app/src/main/kotlin/com/github/oryanmat/trellowidget/activity/GeneralPreferenceFragment.kt
+++ app/src/main/kotlin/com/github/oryanmat/trellowidget/activity/GeneralPreferenceFragment.kt
@@ -28,7 +28,6 @@ class GeneralPreferenceFragment : PreferenceFragment() {
         listener.onSharedPreferenceChanged(preferences, getString(R.string.pref_fore_color_key))
         listener.onSharedPreferenceChanged(preferences, getString(R.string.pref_title_back_color_key))
         listener.onSharedPreferenceChanged(preferences, getString(R.string.pref_title_fore_color_key))
-        listener.onSharedPreferenceChanged(preferences, getString(R.string.pref_title_use_unique_color_key))
         listener.onSharedPreferenceChanged(preferences, getString(R.string.pref_update_interval_key))
         listener.onSharedPreferenceChanged(preferences, getString(R.string.pref_display_board_name_key))
     }
